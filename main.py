9231c057d83912060d39bef202c8a140ecb8c78b
Nikita Babenko <nikita.nikita1234567890@mail.ru>
2020-05-02 22:19:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def send_welcome(message):
 
 @bot.message_handler(content_types=['text'])
 def send_response(message):
-    toxicityLevel = ToxicDetector.resultedToxicity(message)
-    print(message, ": ", toxicityLevel)
+    toxicityLevel = ToxicDetector.resultedToxicity(message.text)
+    print(message.text, ": ", toxicityLevel)
     if toxicityLevel != -1 and toxicityLevel > 0.5:
         response = "Ты че блять, " + message.from_user.username + "? Дружелюбнее сука будь."
         bot.send_message(message.chat.id, response, reply_to_message_id=message.message_id)
