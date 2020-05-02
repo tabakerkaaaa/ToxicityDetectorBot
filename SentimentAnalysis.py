8a9d549ee66c04a59dbfc9ab7dd50801863b3eb4
Nikita Babenko <nikita.nikita1234567890@mail.ru>
2020-05-02 20:55:08 +0300

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import LogisticRegressionCV
