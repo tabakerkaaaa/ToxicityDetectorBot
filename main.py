f64616b31fda2844884fbd0b257ce18d95d5930b
Nikita Babenko <nikita.nikita1234567890@mail.ru>
2020-08-02 02:10:33 +0300

    translated bot's text into English

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ ToxicDetector.fitPipeline()
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Привет, пидорасы, отныне я буду контролировать токсичность в этом чятике")
+    bot.send_message(message.chat.id, "Hello! I'm going to control the toxicity in this chat!")
 
 @bot.message_handler(content_types=['text'])
 def send_response(message):
     toxicityLevel = ToxicDetector.resultedToxicity(message.text)
     print(message.text, ": ", toxicityLevel)
     if toxicityLevel != -1 and toxicityLevel > 0.99:
-        response = "Ты че блять, " + message.from_user.username + "? Дружелюбнее сука будь."
+        response = message.from_user.username + ", please, be more friendly!"
         bot.send_message(message.chat.id, response, reply_to_message_id=message.message_id)
 bot.polling()
